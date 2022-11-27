ad07a3f709f5da9174741ef177e813b2bdc7174a
IanDab47 <dabinett.ian@gmail.com>
2022-11-27 12:01:59 -0600

    adds animation on load

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -6,8 +6,10 @@ import "./style.less"
 export default function Home() {
   return (
     <header>
-      <h1>Welcome to <span className="title-text accent">Global Keebs</span></h1>
-      <div>
+      <h1 style={{ '--order': 1 }}>
+        Welcome to <span className="title-text accent">Global Keebs</span>
+      </h1>
+      <div style={{ '--order': 2 }}>
         <p>Click to Delete Financial Stability</p>
         <button><Link to={'/listings'}>Continue</Link></button>
       </div>

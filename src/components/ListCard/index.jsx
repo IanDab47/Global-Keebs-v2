5c0a6b0ed029338dd23ad934357b1c4b0f0d905e
IanDab47 <dabinett.ian@gmail.com>
2022-11-29 23:17:57 -0600

    fixes padding bug for cards

--- src/components/ListCard/index.jsx
+++ src/components/ListCard/index.jsx
@@ -1,8 +1,44 @@
 // Styles
 import './style.less'
 
-export default function ListCard() {
+export default function ListCard({ currTime, listing, i }) {
+  // Animate grid diagonally
+  // (...) = order
+  // / 13 = rate
+  // + 1.1 = delay -- Must be at least 1
+
+  const rowCardNumber = !window ? null :
+    window.innerWidth > 1660 ? 4 :
+      window.innerWidth > 1252 ? 3 :
+        window.innerWidth > 844 ? 2 : 1
+    
+  const gridAnimMath = (i % rowCardNumber + Math.floor(i / rowCardNumber)) / 13 + 1.1
+
   return (
-    <div>ListCard</div>
+    <div
+      className='card'
+      style={{ '--order': gridAnimMath, '--length': '1.5s', '--flair': listing.flair_text }}
+    >
+      <div className='top'>
+        {!listing.timestamp ?
+          <p>...No Keeb For You!</p>
+          : 
+          <img src='' />
+        }
+
+        <div
+          className='flair'
+          data-color={listing.flair_text}
+        >
+          {listing.flair_text.toLowerCase()}
+        </div>
+      </div>
+
+      <div className='bottom'>
+        <h1>{listing.title}</h1>
+
+        <p>u/{listing.author}</p>
+      </div>
+    </div>
   )
 }

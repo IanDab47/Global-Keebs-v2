ae4e5503a543278301b2b44bfda7fc065aa57304
IanDab47 <dabinett.ian@gmail.com>
2022-12-23 03:01:36 -0600

    adds click position context

--- src/context/ClickContext.jsx
+++ src/context/ClickContext.jsx
@@ -1,17 +1,26 @@
 import { createContext, useContext, useState, useEffect } from "react";
 
 const ClickContext = createContext()
+const ClickPositionContext = createContext()
 
 export const useClick = () => useContext(ClickContext)
+export const useClickPosition = () => useContext(ClickPositionContext)
 
 export const ClickProvider = ({ children }) => {
   const [clickTarget, setClickTarget] = useState(null)
+  const [clickPosition, setClickPosition] = useState({})
+
+  useEffect(() => setClickPosition({
+    xPos: clickTarget?.clientX, yPos: clickTarget?.clientY
+  }), [clickTarget])
 
   return (
-    <ClickContext.Provider value={clickTarget}>
-        <div onClick={e => setClickTarget(e.target)}>
+    <ClickContext.Provider value={clickTarget?.target}>
+      <ClickPositionContext.Provider value={clickPosition}>
+        <div onClick={e => setClickTarget(e)}>
           {children}
         </div>
+      </ClickPositionContext.Provider>
     </ClickContext.Provider>
   )
 }

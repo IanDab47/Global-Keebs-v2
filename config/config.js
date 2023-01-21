f757f2dbb6c51182ae4cc70bbc76c27327c9fb76
IanDab47 <dabinett.ian@gmail.com>
2023-01-21 15:17:26 -0600

    deployment fixes

--- config/config.js
+++ config/config.js
@@ -3,12 +3,12 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: 'sequelize',
-    password: 'sequelize',
-    logging: false,
-    database: 'GlobalKeebs;_v2',
-    host: '127.0.0.1',
-    dialect: 'postgres',
+    'username': 'sequelize',
+    'password': 'sequelize',
+    'logging' : false,
+    'database': 'GlobalKeebs;_v2',
+    'host': '127.0.0.1',
+    'dialect': 'postgres',
   },
   production: {
     use_env_variable: 'DATABASE_URI',
